50fc4d97c3ea1c8ced10dbfda34911d5d1db98b8
pic301 <ppic301@gmail.com>
2020-01-10 01:07:39 +0900

    액션 생성함수  setVisibilityFilter 추가

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,16 @@
 let nextTodoId = 0 
 
-const addTodo = text => ({
+export const addTodo = text => ({
     type: 'ADD_TODO',
     text:text,
 })
 
-const toggleTodo = id => ({
+export const setVisibilityFilter = filter => ({
+    type: 'SET_VISIBILITY_FILTER',
+    filter
+  })
+
+export const toggleTodo = id => ({
     type: 'TOGGLE_TODO',
     id:id,
 })
