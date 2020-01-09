eaf4faf096d20369565a710b7b133249bae36e8b
pic301 <ppic301@gmail.com>
2020-01-09 14:24:37 +0900

    add Todo Component
    propTypes 검증 , Presentational 컴포넌트

---
+++ src/components/Todo.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from 'prop-types'
+const Todo = ({onClick,text,completed}) => (
+  <li
+    onClick={onClick}
+    style={{ textdecoration: completed ? "line-through" : "none" }}
+  >
+    {text}
+  </li>
+)
+Todo.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    completed: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired
+}
+
+export default Todo;
+
+//Todo Component
+//Todo.propTypes
