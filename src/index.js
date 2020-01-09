fd07263984a1538d021089a1c1b51e16253639eb
pic301 <ppic301@gmail.com>
2020-01-10 02:44:04 +0900

    store 추가
    Provier Component 적용

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
+const store = createStore(rootReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
